8cab7965da7b832c569414ba1fed61248f323c8d
Fahad islam <78997963+fahad-islam1@users.noreply.github.com>
2022-10-25 18:48:04 +0500

    Create sorting.dart

---
+++ sorting.dart
@@ -0,0 +1,12 @@
+// Main function
+main() {
+   
+  // Creating List
+  List<int> geeksforgeeks = [13, 2, -11, 142, -389, 32, 3032, 0];
+   
+  // Sorting List
+  geeksforgeeks.sort();
+   
+  // Printing Sorted List
+  print(geeksforgeeks);
+}
